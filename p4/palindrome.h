abeda1e137d58178a2adae953490f2d24014c8c7
wahidrahim <wahidrahim@gmail.com>
2014-07-03 15:13:40 -0400

    solutions 1 - 3

--- p4/palindrome.h
+++
@@ -1,6 +0,0 @@
-#include <stdbool.h>
-
-// is_palindrome(int n) returns true if n is a palindrome number, false otherwise.
-// PRE: 0 <= n <= 999999999
-// POST: true or false
-bool is_palindrome(int n);
