9eca7bf9cea70bcf0f3a3feacb22169b56fb6393
wahidrahim <wahidrahim@gmail.com>
2014-07-25 06:32:09 -0400

    added problem 10 solution

--- P10/P10.java
+++ P10/P10.java
@@ -1,3 +1,10 @@
+/* Problem 10
+ *
+ * The sum of the primes below 10 is 2 + 3 + 5 + 7 = 17.
+ *
+ * Find the sum of all the primes below two million.
+ */
+
 final class P10 {
 	static boolean isPrime(final int n) {
 		final int ROOT = (int) Math.sqrt(n);
