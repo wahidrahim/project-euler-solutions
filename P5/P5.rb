4e75bc11a7ffe7583267ea74d37f38efaebeead2
Wahid Rahim <wahidrahim@gmail.com>
2014-08-31 16:15:48 -0400

    made solution a lot better

--- P5/P5.rb
+++ P5/P5.rb
@@ -1,14 +1,5 @@
-def divisible(n)
-	1.upto(20) do |i|
-		return false if n % i != 0
-	end
-	true
-end
-
-n = 20
-
-until divisible(n) do
-	n += 1
+n = (1..20).reduce(1) do |a, b|
+	a = a.lcm(b)
 end
 
 puts n
